6752890857e3534000258a3124339fd3eaa0dd0b
aleksey <av.pavlow@gmail.com>
2023-12-01 07:17:01 +0300

    Добавляет рабочую версию аудентификации

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,15 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
-})
+Route.group(() => {
+  Route.get('/', async ({ view }) => {
+    return view.render('welcome')
+  })
+}).middleware('auth:web')
+
+Route.get('register', 'AuthController.registerShow').as('auth.register.show')
+Route.post('register', 'AuthController.register').as('auth.register') // ++
+Route.get('login', 'AuthController.loginShow').as('auth.login.show')
+Route.post('login', 'AuthController.login').as('auth.login')          // ++
+Route.get('logout', 'AuthController.logout').as('auth.logout')
+
