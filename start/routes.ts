0c3f6d06bbe45d1a75446fa8af9772d5044204a2
aleksey <av.pavlow@gmail.com>
2023-12-01 16:58:09 +0300

    Добавляет layout

--- start/routes.ts
+++ start/routes.ts
@@ -22,7 +22,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
   Route.get('/', async ({ view }) => {
-    return view.render('welcome')
+    return view.render('home')
   })
 }).middleware('auth:web')
 
